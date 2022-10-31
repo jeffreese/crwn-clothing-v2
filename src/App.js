db54ce84654154c0206ec05add6d95563a8e518d
Jeff Reese <4625946+jeffreese@users.noreply.github.com>
2022-10-30 20:20:21 -0600

    adds basic navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import Categories from "./components/categories/categories.component"
+import { Routes, Route } from "react-router-dom"
+
+import Navigation from "./routes/navigation/navigation.component"
+import Home from "./routes/home/home.component"
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+}
 
 const App = () => {
-  return <Categories />
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
