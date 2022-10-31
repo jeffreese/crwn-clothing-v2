efff69194a306f003dbddee9a98231c3845e6c5e
Jeff Reese <4625946+jeffreese@users.noreply.github.com>
2022-10-30 21:23:43 -0600

    added sign in functionality

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth"
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyBMZM_Mt3XwkHf1UOsvVEO28nAwtScw69g",
@@ -22,3 +23,31 @@ provider.setCustomParameters({ prompt: "select_account" })
 
 export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid)
+
+  console.log(userDocRef)
+
+  const userSnapshot = await getDoc(userDocRef)
+  console.log(userSnapshot)
+  console.log(userSnapshot.exists())
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      })
+    } catch (error) {
+      console.log("Error creating user", error.message)
+    }
+  }
+  return userDocRef
+}
